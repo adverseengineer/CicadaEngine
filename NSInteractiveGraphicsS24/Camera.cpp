47c238dc73431d83c8f51c5a92a941b939d0f9db
Nick Sells <adverseengineer@gmail.com>
2024-03-09 17:56:47 -0500

    refactored Camera.h and Camera.cpp

--- NSInteractiveGraphicsS24/Camera.cpp
+++ NSInteractiveGraphicsS24/Camera.cpp
@@ -1,10 +1,9 @@
 #include "Camera.h"
-
 #include <ext/matrix_transform.hpp>
 
-glm::mat4 Camera::LookForward(void) const {
+glm::mat4 Camera::LookForward() const {
 	glm::vec3 camPos = referenceFrame[3];
-	glm::vec3 camForward = -lookFrame[2];
+	glm::vec3 camForward = -lookFrame[2]; //negative z axis of the camera's local coordinate system
 	glm::vec3 camTarget = camPos + camForward;
 	glm::vec3 camUp = lookFrame[1];
 	return glm::lookAt(camPos, camTarget, camUp);
