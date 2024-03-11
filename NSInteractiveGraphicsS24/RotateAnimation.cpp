f0061daa28a87b823e83c49e249d5d83def9aef0
Nick Sells <adverseengineer@gmail.com>
2024-03-11 00:18:51 -0400

    refactored RotateAnimation.h and RotateAnimation.cpp

--- NSInteractiveGraphicsS24/RotateAnimation.cpp
+++ NSInteractiveGraphicsS24/RotateAnimation.cpp
@@ -3,12 +3,10 @@
 #include <glm/gtc/matrix_transform.hpp>
 
 void RotateAnimation::Update(double elapsedSeconds) {
-
 	if (object == nullptr)
 		return;
-
 	float deltaSpeed = static_cast<float>(speed * elapsedSeconds);
-	glm::mat4 referenceFrame = object->GetReferenceFrame();
+	glm::mat4 referenceFrame = object->GetLocalReferenceFrame();
 	referenceFrame = glm::rotate(referenceFrame, glm::radians(deltaSpeed), axis);
-	object->SetReferenceFrame(referenceFrame);
+	object->SetLocalReferenceFrame(referenceFrame);
 }
