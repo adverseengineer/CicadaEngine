4383f1622875b6a417c21de07a1194cd9d8e104d
Nick Sells <adverseengineer@gmail.com>
2024-03-10 23:06:05 -0400

    refactored ObjectManager.h and ObjectManager.cpp

--- NSInteractiveGraphicsS24/ObjectManager.cpp
+++ NSInteractiveGraphicsS24/ObjectManager.cpp
@@ -1,17 +1,26 @@
 #include "ObjectManager.h"
 
-void ObjectManager::SetObject(const std::string& key, const std::shared_ptr<GraphicsObject>& obj) {
-	objectMap.insert_or_assign(key, obj);
-}
-
-std::shared_ptr<GraphicsObject> ObjectManager::GetObject(const std::string& key) const {
-	assert(objectMap.contains(key)); //TODO: remove this at some point and handle it
+const std::shared_ptr<GraphicsObject>& ObjectManager::GetObject(const std::string& key) const {
+	//TODO: handle the possible out_of_range exception here
 	return objectMap.at(key);
 }
 
+bool ObjectManager::AddObject(const std::string& key, const std::shared_ptr<GraphicsObject>& obj) {
+	const auto& result = objectMap.insert({key, obj});
+	return result.second;
+}
+
+bool ObjectManager::SetObject(const std::string& key, const std::shared_ptr<GraphicsObject>& obj) {
+	if(objectMap.contains(key)) {
+		objectMap.at(key) = obj;
+		return true;
+	}
+	else
+		return false;
+}
+
 void ObjectManager::Update(double elapsedSeconds) {
-	
-	for (auto& pair : objectMap) {
-		pair.second->Update(elapsedSeconds);
+	for (auto& [key, obj] : objectMap) {
+		obj->Update(elapsedSeconds);
 	}
 }
