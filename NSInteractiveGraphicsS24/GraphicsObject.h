6c92847372f4efae86a6f31be9f5119c88ddf1c1
Nick Sells <adverseengineer@gmail.com>
2024-03-10 22:14:10 -0400

    removed auto methods from GraphicsObject.h

--- NSInteractiveGraphicsS24/GraphicsObject.h
+++ NSInteractiveGraphicsS24/GraphicsObject.h
@@ -31,27 +31,27 @@ public:
 	}
 
 	//returns the reference frame of this object, regardless of whether or not it is parented
-	inline const auto& GetLocalReferenceFrame() const { return referenceFrame; }
+	inline const glm::mat4& GetLocalReferenceFrame() const { return referenceFrame; }
 	
 	//sets the reference frame of this object, not taking into account the parent
 	inline void SetLocalReferenceFrame(const glm::mat4& referenceFrame) { this->referenceFrame = referenceFrame; }
 	
-	inline const auto& GetVertexBuffer() const { return vertBuf; }
+	inline const std::shared_ptr<VertexBuffer>& GetVertexBuffer() const { return vertBuf; }
 	inline void SetVertexBuffer(const std::shared_ptr<VertexBuffer>& vertBuf) { this->vertBuf = vertBuf; }
 
 	//returns a const reference to the child container
-	inline const auto& GetChildren() const { return children; }
+	inline const std::unordered_set<std::shared_ptr<GraphicsObject>>& GetChildren() const { return children; }
 
 	//inserts a new child under this object, returns true if success, false if already present
 	bool AddChild(const std::shared_ptr<GraphicsObject>& child);
 
-	inline const auto& GetAnimation() const { return animation; }
+	inline const std::shared_ptr<Animation>& GetAnimation() const { return animation; }
 	inline void SetAnimation(const std::shared_ptr<Animation>& animation) { this->animation = animation; }
 
-	inline const auto& GetMaterial() const { return material; }
+	inline const std::shared_ptr<Material>& GetMaterial() const { return material; }
 	inline void SetMaterial(const std::shared_ptr<Material>& material) { this->material = material; }
 
-	const auto& GetPosition() const;
+	const glm::vec3& GetPosition() const;
 	void SetPosition(const glm::vec3& position);
 
 	void ResetOrientation();
