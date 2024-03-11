02e1f6e13abbe22913265c9f7ce1ff88ef0dfefb
Nick Sells <adverseengineer@gmail.com>
2024-03-10 22:12:36 -0400

    refactored Animation.h

--- NSInteractiveGraphicsS24/Animation.h
+++ NSInteractiveGraphicsS24/Animation.h
@@ -13,7 +13,7 @@ public:
 	inline Animation() : object(nullptr) {};
 	inline virtual ~Animation() = default;
 
-	inline const auto& GetObject() const { return object; }
+	inline const std::shared_ptr<GraphicsObject>& GetObject() const { return object; }
 	inline void SetObject(const std::shared_ptr<GraphicsObject>& object) { this->object = object; }
 	
 	inline virtual void Update(double elapsedSeconds) = 0; //any class with at least one virtual method set to 0 is abstract
