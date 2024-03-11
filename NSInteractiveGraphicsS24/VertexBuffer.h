0c0c43e9d77ec5feb724520504eb9df75c08ff15
Nick Sells <adverseengineer@gmail.com>
2024-03-10 23:39:49 -0400

    refactored VertexBuffer.h and VertexBuffer.cpp

--- NSInteractiveGraphicsS24/VertexBuffer.h
+++ NSInteractiveGraphicsS24/VertexBuffer.h
@@ -1,10 +1,9 @@
 #pragma once
-#include <glad/glad.h> 
-#include <vector>
-#include <unordered_map>
-#include <string>
-
 #include "texture.h"
+#include <glad/glad.h> 
+#include <string>
+#include <unordered_map>
+#include <vector>
 
 struct VertexAttribute {
 	unsigned int index;
@@ -24,26 +23,29 @@ protected:
 	std::vector<float> vertexData;
 	std::unordered_map<std::string, VertexAttribute> attributeMap;
 	int textureUnit;
-	std::shared_ptr<Texture> texturePtr;
+	std::shared_ptr<Texture> texture;
 
 public:
 	VertexBuffer(unsigned int numElementsPerVertex = 3);
 	~VertexBuffer();
 
-	inline void Select() { glBindBuffer(GL_ARRAY_BUFFER, vboId); }
-	inline void Deselect() { glBindBuffer(GL_ARRAY_BUFFER, 0); }
 	inline unsigned int GetNumberOfVertices() const { return numberOfVertices; }
+
 	inline int GetPrimitiveType() const { return primitiveType; }
 	inline void SetPrimitiveType(int primitiveType) { this->primitiveType = primitiveType; }
-	inline int GetTextureUnit(void) const { return textureUnit; }
-	inline void SetTextureUnit(int textureUnit) { this->textureUnit = textureUnit; }
-	inline std::shared_ptr<Texture> GetTexturePtr(void) const { return texturePtr; }
-	inline void SetTexturePtr(std::shared_ptr<Texture> texturePtr) { this->texturePtr = texturePtr; }
-	inline bool HasTexture(void) const { return texturePtr != nullptr; }
 	
-	void SelectTexture(void) const;
+	inline int GetTextureUnit() const { return textureUnit; }
+	inline void SetTextureUnit(int textureUnit) { this->textureUnit = textureUnit; }
+	
+	inline const std::shared_ptr<Texture>& GetTexture() const { return texture; }
+	inline void SetTexture(const std::shared_ptr<Texture>& texture) { this->texture = texture; }
+	inline bool HasTexture() const { return texture != nullptr; }
+
+	void Select() const;
+	void Deselect() const;
+	void SelectTexture() const;
 	void AddVertexData(unsigned int count, ...);
-	void StaticAllocate();
+	void StaticAllocate() const;
 	void AddVertexAttribute(const std::string& name, unsigned int index, unsigned int numberOfElements, unsigned int offsetCount = 0);
-	void SetUpAttributeInterpretration();
+	void SetUpAttributeInterpretration() const;
 };
