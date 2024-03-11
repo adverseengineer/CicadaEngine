45f128532ae7232d7bc81653461c9bd016c4ea0a
Nick Sells <adverseengineer@gmail.com>
2024-03-11 00:50:32 -0400

    refactored Timer.h and Timer.cpp

--- NSInteractiveGraphicsS24/Timer.cpp
+++ NSInteractiveGraphicsS24/Timer.cpp
@@ -1,20 +1,16 @@
 #include "Timer.h"
 
-Timer::Timer()
-{
+Timer::Timer() {
 	StartTiming();
 }
 
-void Timer::StartTiming()
-{
+void Timer::StartTiming() {
 	startTime = std::chrono::high_resolution_clock::now();
 }
 
-double Timer::GetElapsedTimeInSeconds()
-{
+double Timer::GetElapsedTimeInSeconds() {
 	endTime = std::chrono::high_resolution_clock::now();
-	std::chrono::duration<double> timeSpan =
-		duration_cast<std::chrono::duration<double>>(endTime - startTime);
+	std::chrono::duration<double> timeSpan = duration_cast<std::chrono::duration<double>>(endTime - startTime);
 	startTime = std::chrono::high_resolution_clock::now();
 	return timeSpan.count();
 }
