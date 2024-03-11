c2610263d096938f69b5ef0caa6a77d3fbeaceda
Nick Sells <adverseengineer@gmail.com>
2024-03-11 00:51:58 -0400

    refactored util.h

--- NSInteractiveGraphicsS24/Util.h
+++ NSInteractiveGraphicsS24/Util.h
@@ -1,23 +1,20 @@
 #pragma once
-
 #include <sstream>
 #include <string>
 #include <vector>
 
 class Util {
-
 private:
 	static std::stringstream _log;
 
 public:
-
 	inline static void Trim(std::string& str) {
 		const std::string delimiters = " \f\n\r\t\v";
 		str.erase(str.find_last_not_of(delimiters) + 1);
 		str.erase(0, str.find_first_not_of(delimiters));
 	}
 
-	inline static const std::string GetLog(void) {
+	inline static const std::string& GetLog(void) {
 		return _log.str();
 	}
 	
