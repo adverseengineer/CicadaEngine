4dfc31761bd87961096e87be5bb93e6d8a9c7dde
Nick Sells <adverseengineer@gmail.com>
2024-03-20 11:42:29 -0400

    added XY planes to generate.cpp and generate.h

--- NSInteractiveGraphicsS24/Generate.cpp
+++ NSInteractiveGraphicsS24/Generate.cpp
@@ -1,11 +1,11 @@
 #include "Generate.h"
 
-#define up 0.0f, 1.0f, 0.0f
-#define down 0.0f, -1.0f, 0.0f
-#define left -1.0f, 0.0f, 0.0f
-#define right 1.0f, 0.0f, 0.0f
-#define forward 0.0f, 0.0f, -1.0f
-#define backward 0.0f, 0.0f, 1.0f
+#define NORM_UP 0.0f, 1.0f, 0.0f
+#define NORM_DOWN 0.0f, -1.0f, 0.0f
+#define NORM_LEFT -1.0f, 0.0f, 0.0f
+#define NORM_RIGHT 1.0f, 0.0f, 0.0f
+#define NORM_FORWARD 0.0f, 0.0f, -1.0f
+#define NORM_BACKWARD 0.0f, 0.0f, 1.0f
 
 std::shared_ptr<VertexBuffer> Generate::Cuboid(float width, float height, float depth, glm::vec3 color, glm::vec2 tex) {
 
@@ -83,47 +83,47 @@ std::shared_ptr<VertexBuffer> Generate::CuboidWithNormals(float width, float hei
 	float hd = depth / 2;
 
 	//front
-	vertBuf->AddVertexData(12, -hw, hh, hd, color.r, color.g, color.b, color.a, backward, 0.0f, tex.t);
-	vertBuf->AddVertexData(12, -hw, -hh, hd, color.r, color.g, color.b, color.a, backward, 0.0f, 0.0f);
-	vertBuf->AddVertexData(12, hw, -hh, hd, color.r, color.g, color.b, color.a, backward, tex.s, 0.0f);
-	vertBuf->AddVertexData(12, -hw, hh, hd, color.r, color.g, color.b, color.a, backward, 0.0f, tex.t);
-	vertBuf->AddVertexData(12, hw, -hh, hd, color.r, color.g, color.b, color.a, backward, tex.s, 0.0f);
-	vertBuf->AddVertexData(12, hw, hh, hd, color.r, color.g, color.b, color.a, backward, tex.s, tex.t);
+	vertBuf->AddVertexData(12, -hw, hh, hd, color.r, color.g, color.b, color.a, NORM_BACKWARD, 0.0f, tex.t);
+	vertBuf->AddVertexData(12, -hw, -hh, hd, color.r, color.g, color.b, color.a, NORM_BACKWARD, 0.0f, 0.0f);
+	vertBuf->AddVertexData(12, hw, -hh, hd, color.r, color.g, color.b, color.a, NORM_BACKWARD, tex.s, 0.0f);
+	vertBuf->AddVertexData(12, -hw, hh, hd, color.r, color.g, color.b, color.a, NORM_BACKWARD, 0.0f, tex.t);
+	vertBuf->AddVertexData(12, hw, -hh, hd, color.r, color.g, color.b, color.a, NORM_BACKWARD, tex.s, 0.0f);
+	vertBuf->AddVertexData(12, hw, hh, hd, color.r, color.g, color.b, color.a, NORM_BACKWARD, tex.s, tex.t);
 	//right
-	vertBuf->AddVertexData(12, hw, hh, hd, color.r, color.g, color.b, color.a, right, 0.0f, tex.t);
-	vertBuf->AddVertexData(12, hw, -hh, hd, color.r, color.g, color.b, color.a, right, 0.0f, 0.0f);
-	vertBuf->AddVertexData(12, hw, -hh, -hd, color.r, color.g, color.b, color.a, right, tex.s, 0.0f);
-	vertBuf->AddVertexData(12, hw, hh, hd, color.r, color.g, color.b, color.a, right, 0.0f, tex.t);
-	vertBuf->AddVertexData(12, hw, -hh, -hd, color.r, color.g, color.b, color.a, right, tex.s, 0.0f);
-	vertBuf->AddVertexData(12, hw, hh, -hd, color.r, color.g, color.b, color.a, right, tex.s, tex.t);
+	vertBuf->AddVertexData(12, hw, hh, hd, color.r, color.g, color.b, color.a, NORM_RIGHT, 0.0f, tex.t);
+	vertBuf->AddVertexData(12, hw, -hh, hd, color.r, color.g, color.b, color.a, NORM_RIGHT, 0.0f, 0.0f);
+	vertBuf->AddVertexData(12, hw, -hh, -hd, color.r, color.g, color.b, color.a, NORM_RIGHT, tex.s, 0.0f);
+	vertBuf->AddVertexData(12, hw, hh, hd, color.r, color.g, color.b, color.a, NORM_RIGHT, 0.0f, tex.t);
+	vertBuf->AddVertexData(12, hw, -hh, -hd, color.r, color.g, color.b, color.a, NORM_RIGHT, tex.s, 0.0f);
+	vertBuf->AddVertexData(12, hw, hh, -hd, color.r, color.g, color.b, color.a, NORM_RIGHT, tex.s, tex.t);
 	//back
-	vertBuf->AddVertexData(12, hw, hh, -hd, color.r, color.g, color.b, color.a, forward, 0.0f, tex.t);
-	vertBuf->AddVertexData(12, hw, -hh, -hd, color.r, color.g, color.b, color.a, forward, 0.0f, 0.0f);
-	vertBuf->AddVertexData(12, -hw, -hh, -hd, color.r, color.g, color.b, color.a, forward, tex.s, 0.0f);
-	vertBuf->AddVertexData(12, hw, hh, -hd, color.r, color.g, color.b, color.a, forward, 0.0f, tex.t);
-	vertBuf->AddVertexData(12, -hw, -hh, -hd, color.r, color.g, color.b, color.a, forward, tex.s, 0.0f);
-	vertBuf->AddVertexData(12, -hw, hh, -hd, color.r, color.g, color.b, color.a, forward, tex.s, tex.t);
+	vertBuf->AddVertexData(12, hw, hh, -hd, color.r, color.g, color.b, color.a, NORM_FORWARD, 0.0f, tex.t);
+	vertBuf->AddVertexData(12, hw, -hh, -hd, color.r, color.g, color.b, color.a, NORM_FORWARD, 0.0f, 0.0f);
+	vertBuf->AddVertexData(12, -hw, -hh, -hd, color.r, color.g, color.b, color.a, NORM_FORWARD, tex.s, 0.0f);
+	vertBuf->AddVertexData(12, hw, hh, -hd, color.r, color.g, color.b, color.a, NORM_FORWARD, 0.0f, tex.t);
+	vertBuf->AddVertexData(12, -hw, -hh, -hd, color.r, color.g, color.b, color.a, NORM_FORWARD, tex.s, 0.0f);
+	vertBuf->AddVertexData(12, -hw, hh, -hd, color.r, color.g, color.b, color.a, NORM_FORWARD, tex.s, tex.t);
 	//left
-	vertBuf->AddVertexData(12, -hw, hh, -hd, color.r, color.g, color.b, color.a, left, 0.0f, tex.t);
-	vertBuf->AddVertexData(12, -hw, -hh, -hd, color.r, color.g, color.b, color.a, left, 0.0f, 0.0f);
-	vertBuf->AddVertexData(12, -hw, -hh, hd, color.r, color.g, color.b, color.a, left, tex.s, 0.0f);
-	vertBuf->AddVertexData(12, -hw, hh, -hd, color.r, color.g, color.b, color.a, left, 0.0f, tex.t);
-	vertBuf->AddVertexData(12, -hw, -hh, hd, color.r, color.g, color.b, color.a, left, tex.s, 0.0f);
-	vertBuf->AddVertexData(12, -hw, hh, hd, color.r, color.g, color.b, color.a, left, tex.s, tex.t);
+	vertBuf->AddVertexData(12, -hw, hh, -hd, color.r, color.g, color.b, color.a, NORM_LEFT, 0.0f, tex.t);
+	vertBuf->AddVertexData(12, -hw, -hh, -hd, color.r, color.g, color.b, color.a, NORM_LEFT, 0.0f, 0.0f);
+	vertBuf->AddVertexData(12, -hw, -hh, hd, color.r, color.g, color.b, color.a, NORM_LEFT, tex.s, 0.0f);
+	vertBuf->AddVertexData(12, -hw, hh, -hd, color.r, color.g, color.b, color.a, NORM_LEFT, 0.0f, tex.t);
+	vertBuf->AddVertexData(12, -hw, -hh, hd, color.r, color.g, color.b, color.a, NORM_LEFT, tex.s, 0.0f);
+	vertBuf->AddVertexData(12, -hw, hh, hd, color.r, color.g, color.b, color.a, NORM_LEFT, tex.s, tex.t);
 	//top
-	vertBuf->AddVertexData(12, -hw, hh, -hd, color.r, color.g, color.b, color.a, up, 0.0f, tex.t);
-	vertBuf->AddVertexData(12, -hw, hh, hd, color.r, color.g, color.b, color.a, up, 0.0f, 0.0f);
-	vertBuf->AddVertexData(12, hw, hh, hd, color.r, color.g, color.b, color.a, up, tex.s, 0.0f);
-	vertBuf->AddVertexData(12, -hw, hh, -hd, color.r, color.g, color.b, color.a, up, 0.0f, tex.t);
-	vertBuf->AddVertexData(12, hw, hh, hd, color.r, color.g, color.b, color.a, up, tex.s, 0.0f);
-	vertBuf->AddVertexData(12, hw, hh, -hd, color.r, color.g, color.b, color.a, up, tex.s, tex.t);
+	vertBuf->AddVertexData(12, -hw, hh, -hd, color.r, color.g, color.b, color.a, NORM_UP, 0.0f, tex.t);
+	vertBuf->AddVertexData(12, -hw, hh, hd, color.r, color.g, color.b, color.a, NORM_UP, 0.0f, 0.0f);
+	vertBuf->AddVertexData(12, hw, hh, hd, color.r, color.g, color.b, color.a, NORM_UP, tex.s, 0.0f);
+	vertBuf->AddVertexData(12, -hw, hh, -hd, color.r, color.g, color.b, color.a, NORM_UP, 0.0f, tex.t);
+	vertBuf->AddVertexData(12, hw, hh, hd, color.r, color.g, color.b, color.a, NORM_UP, tex.s, 0.0f);
+	vertBuf->AddVertexData(12, hw, hh, -hd, color.r, color.g, color.b, color.a, NORM_UP, tex.s, tex.t);
 	//bottom
-	vertBuf->AddVertexData(12, hw, -hh, -hd, color.r, color.g, color.b, color.a, down, 0.0f, tex.t);
-	vertBuf->AddVertexData(12, hw, -hh, hd, color.r, color.g, color.b, color.a, down, 0.0f, 0.0f);
-	vertBuf->AddVertexData(12, -hw, -hh, hd, color.r, color.g, color.b, color.a, down, tex.s, 0.0f);
-	vertBuf->AddVertexData(12, hw, -hh, -hd, color.r, color.g, color.b, color.a, down, 0.0f, tex.t);
-	vertBuf->AddVertexData(12, -hw, -hh, hd, color.r, color.g, color.b, color.a, down, tex.s, 0.0f);
-	vertBuf->AddVertexData(12, -hw, -hh, -hd, color.r, color.g, color.b, color.a, down, tex.s, tex.t);
+	vertBuf->AddVertexData(12, hw, -hh, -hd, color.r, color.g, color.b, color.a, NORM_DOWN, 0.0f, tex.t);
+	vertBuf->AddVertexData(12, hw, -hh, hd, color.r, color.g, color.b, color.a, NORM_DOWN, 0.0f, 0.0f);
+	vertBuf->AddVertexData(12, -hw, -hh, hd, color.r, color.g, color.b, color.a, NORM_DOWN, tex.s, 0.0f);
+	vertBuf->AddVertexData(12, hw, -hh, -hd, color.r, color.g, color.b, color.a, NORM_DOWN, 0.0f, tex.t);
+	vertBuf->AddVertexData(12, -hw, -hh, hd, color.r, color.g, color.b, color.a, NORM_DOWN, tex.s, 0.0f);
+	vertBuf->AddVertexData(12, -hw, -hh, -hd, color.r, color.g, color.b, color.a, NORM_DOWN, tex.s, tex.t);
 	#pragma endregion
 
 	//add vertex attributes to the vertex buffer
@@ -135,7 +135,7 @@ std::shared_ptr<VertexBuffer> Generate::CuboidWithNormals(float width, float hei
 	return vertBuf;
 }
 
-std::shared_ptr<VertexBuffer> Generate::Plane(float width, float depth, glm::vec3 color, glm::vec2 tex) {
+std::shared_ptr<VertexBuffer> Generate::PlaneXZ(float width, float depth, glm::vec3 color, glm::vec2 tex) {
 	
 	auto vertBuf = std::make_shared<VertexBuffer>(8);
 
@@ -157,19 +157,66 @@ std::shared_ptr<VertexBuffer> Generate::Plane(float width, float depth, glm::vec
 	return vertBuf;
 }
 
-std::shared_ptr<VertexBuffer> Generate::PlaneWithNormals(float width, float depth, glm::vec4 color, glm::vec2 tex) {
+std::shared_ptr<VertexBuffer> Generate::PlaneXY(float width, float height, glm::vec3 color, glm::vec2 tex) {
+
+	auto vertBuf = std::make_shared<VertexBuffer>(8);
+
+	float hw = width / 2;
+	float hh = height / 2;
+
+	vertBuf->AddVertexData(8, -hw, -hh, 0, color.r, color.g, color.b, 0.0f, tex.t);
+	vertBuf->AddVertexData(8, -hw, hh, 0, color.r, color.g, color.b, 0.0f, 0.0f);
+	vertBuf->AddVertexData(8, hw, hh, 0, color.r, color.g, color.b, tex.s, 0.0f);
+	vertBuf->AddVertexData(8, -hw, -hh, 0, color.r, color.g, color.b, 0.0f, tex.t);
+	vertBuf->AddVertexData(8, hw, hh, 0, color.r, color.g, color.b, tex.s, 0.0f);
+	vertBuf->AddVertexData(8, hw, -hh, 0, color.r, color.g, color.b, tex.s, tex.t);
+
+	//add vertex attributes to the vertex buffer
+	vertBuf->AddVertexAttribute("position", 0, 3, 0);
+	vertBuf->AddVertexAttribute("vertexColor", 1, 3, 3);
+	vertBuf->AddVertexAttribute("texCoord", 2, 2, 6);
+
+	return vertBuf;
+}
+
+std::shared_ptr<VertexBuffer> Generate::PlaneXZWithNormals(float width, float depth, glm::vec4 color, glm::vec2 tex) {
 
 	auto vertBuf = std::make_shared<VertexBuffer>(12);
 
 	float hw = width / 2;
 	float hd = depth / 2;
 
-	vertBuf->AddVertexData(12, -hw, 0, -hd, color.r, color.g, color.b, color.a, up, 0.0f, tex.t);
-	vertBuf->AddVertexData(12, -hw, 0, hd, color.r, color.g, color.b, color.a, up, 0.0f, 0.0f);
-	vertBuf->AddVertexData(12, hw, 0, hd, color.r, color.g, color.b, color.a, up, tex.s, 0.0f);
-	vertBuf->AddVertexData(12, -hw, 0, -hd, color.r, color.g, color.b, color.a, up, 0.0f, tex.t);
-	vertBuf->AddVertexData(12, hw, 0, hd, color.r, color.g, color.b, color.a, up, tex.s, 0.0f);
-	vertBuf->AddVertexData(12, hw, 0, -hd, color.r, color.g, color.b, color.a, up, tex.s, tex.t);
+	vertBuf->AddVertexData(12, -hw, 0, -hd, color.r, color.g, color.b, color.a, NORM_UP, 0.0f, tex.t);
+	vertBuf->AddVertexData(12, -hw, 0, hd, color.r, color.g, color.b, color.a, NORM_UP, 0.0f, 0.0f);
+	vertBuf->AddVertexData(12, hw, 0, hd, color.r, color.g, color.b, color.a, NORM_UP, tex.s, 0.0f);
+	vertBuf->AddVertexData(12, -hw, 0, -hd, color.r, color.g, color.b, color.a, NORM_UP, 0.0f, tex.t);
+	vertBuf->AddVertexData(12, hw, 0, hd, color.r, color.g, color.b, color.a, NORM_UP, tex.s, 0.0f);
+	vertBuf->AddVertexData(12, hw, 0, -hd, color.r, color.g, color.b, color.a, NORM_UP, tex.s, tex.t);
+
+	//add vertex attributes to the vertex buffer
+	vertBuf->AddVertexAttribute("position", 0, 3, 0);
+	vertBuf->AddVertexAttribute("vertexColor", 1, 4, 3);
+	vertBuf->AddVertexAttribute("vertexNormal", 2, 3, 7);
+	vertBuf->AddVertexAttribute("texCoord", 3, 2, 10);
+
+	return vertBuf;
+}
+
+std::shared_ptr<VertexBuffer> Generate::PlaneXYWithNormals(float width, float height, glm::vec4 color, glm::vec2 tex) {
+
+	auto vertBuf = std::make_shared<VertexBuffer>(12);
+
+	float hw = width / 2;
+	float hh = height / 2;
+
+
+
+	vertBuf->AddVertexData(12, -hw, -hh, 0, color.r, color.g, color.b, color.a, NORM_FORWARD, 0.0f, tex.t);
+	vertBuf->AddVertexData(12, -hw, hh, 0, color.r, color.g, color.b, color.a, NORM_FORWARD, 0.0f, 0.0f);
+	vertBuf->AddVertexData(12, hw, hh, 0, color.r, color.g, color.b, color.a, NORM_FORWARD, tex.s, 0.0f);
+	vertBuf->AddVertexData(12, -hw, -hh, 0, color.r, color.g, color.b, color.a, NORM_FORWARD, 0.0f, tex.t);
+	vertBuf->AddVertexData(12, hw, hh, 0, color.r, color.g, color.b, color.a, NORM_FORWARD, tex.s, 0.0f);
+	vertBuf->AddVertexData(12, hw, -hh, 0, color.r, color.g, color.b, color.a, NORM_FORWARD, tex.s, tex.t);
 
 	//add vertex attributes to the vertex buffer
 	vertBuf->AddVertexAttribute("position", 0, 3, 0);
