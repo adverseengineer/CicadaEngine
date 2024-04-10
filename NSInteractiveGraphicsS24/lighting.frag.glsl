d6cdbfdbfbf3dad8ef2c2537c4d50bb238c9a0a4
Nick Sells <adverseengineer@gmail.com>
2024-04-10 00:36:13 -0400

    small shader whitespace changes

--- NSInteractiveGraphicsS24/lighting.frag.glsl
+++ NSInteractiveGraphicsS24/lighting.frag.glsl
@@ -33,34 +33,27 @@ void main()
    vec3 viewDir = normalize(viewPosition - fragPosition);
    vec4 specular;
    vec3 toLocalLightDir = normalize(localLightPosition - fragPosition);
-   vec4 localDiffuse = 
-      calculateDiffuse(
-         toLocalLightDir, fragNormal, localLightIntensity, localLightColor);
-
+   vec4 localDiffuse = calculateDiffuse(toLocalLightDir, fragNormal, localLightIntensity, localLightColor);
    float distanceToLight = length(localLightPosition - fragPosition);
-   float attenuation = 1.0 / 
-      (1.0 + localLightAttenuationCoef * pow(distanceToLight, 2));
+   float attenuation = 1.0 / (1.0 + localLightAttenuationCoef * pow(distanceToLight, 2));
    localDiffuse *= attenuation;
    if(materialShininess > 0.0){
       vec3 reflectDir = reflect(-toLocalLightDir, fragNormal);
       float angIncidence = max(dot(viewDir, reflectDir), 0.0f);
       float spec = pow(angIncidence, materialShininess);
-      specular = materialSpecularIntensity * spec * 
-        localLightIntensity * vec4(localLightColor, 1.0f); 
+      specular = materialSpecularIntensity * spec * localLightIntensity * vec4(localLightColor, 1.0f); 
       
       vec3 globalReflectDir = reflect(-toGlobalLightDir, fragNormal);
       angIncidence = max(dot(viewDir, globalReflectDir), 0.0f);
       spec = pow(angIncidence, materialShininess);
-      specular += materialSpecularIntensity * spec * 
-        globalLightIntensity * vec4(globalLightColor, 1.0f);
+      specular += materialSpecularIntensity * spec * globalLightIntensity * vec4(globalLightColor, 1.0f);
       specular.a = 1.0f;
    }
 
    vec4 texFragColor = texture(tex, fragTexCoord) * fragColor;
    vec4 ambientColor = materialAmbientIntensity * vec4(1.0f, 1.0f, 1.0f, 1.0f);
    ambientColor.a = 1.0f;
-   color = (ambientColor + globalDiffuse + localDiffuse + specular) *   
-            texFragColor;
+   color = (ambientColor + globalDiffuse + localDiffuse + specular) * texFragColor;
 }
 
 vec4 calculateDiffuse(vec3 lightDir, vec3 unitNormal, float lightIntensity, vec3 lightColor)
--- NSInteractiveGraphicsS24/texture.frag.glsl
+++ NSInteractiveGraphicsS24/texture.frag.glsl
@@ -2,8 +2,8 @@
 in vec4 fragColor;
 in vec2 fragTexCoord;
 out vec4 color;
-uniform sampler2D texUnit;
+uniform sampler2D tex;
 void main() {
-	vec4 texFragColor = texture(texUnit, fragTexCoord) * fragColor;
+	vec4 texFragColor = texture(tex, fragTexCoord) * fragColor;
 	color = texFragColor;
 }
