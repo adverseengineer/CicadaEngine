cd64ecb8898fed2c8fe4aab04cf874a223d65a72
Nick Sells <adverseengineer@gmail.com>
2024-09-30 05:32:47 -0400

    moved VAO's into Mesh from Renderer also removed a ton of unneeded calls from Renderer

--- NSInteractiveGraphicsS24/Mesh.h
+++ NSInteractiveGraphicsS24/Mesh.h
@@ -8,14 +8,21 @@ struct Mesh {
 	VertexBuffer m_vBuf;
 	IndexBuffer m_iBuf;
 
-	Mesh() : m_vBuf(), m_iBuf() {
-	}
+	unsigned int m_vaoId = 0;
 
 	inline Mesh(unsigned int numElemsPerVertex = 3) : m_vBuf(numElemsPerVertex), m_iBuf() {
+		glGenVertexArrays(1, &m_vaoId);
+	}
+
+	inline ~Mesh() {
+		glDeleteVertexArrays(1, &m_vaoId);
 	}
 
 	Mesh(const Mesh&) = delete;
 	Mesh& operator=(const Mesh&) = delete;
+
+	inline void Bind() { glBindVertexArray(m_vaoId); }
+	inline void Unbind() { glBindVertexArray(0); }
 
 	inline VertexBuffer& GetVertexBuffer() { return m_vBuf; }
 	inline IndexBuffer& GetIndexBuffer() { return m_iBuf; }
