634bd86b1b79278206648e7179a1521640863aaa
Nick Sells <adverseengineer@gmail.com>
2025-03-11 02:00:36 -0400

    added ecs inspection debug function

--- EngineCore/src/Main.cpp
+++ EngineCore/src/Main.cpp
@@ -42,22 +42,59 @@ inline void SetupRegistry(entt::registry& reg) {
 
 	auto crate = reg.create();
 	names.emplace(crate, "crate");
-	reg.emplace<TransformComponent>(crate, glm::mat4{1.0});
+	reg.emplace<TransformComponent>(crate, glm::vec3{ 10.0, 0.0, 10.0 });
 	reg.emplace<MeshComponent>(crate, crateMesh);
 	reg.emplace<MaterialComponent>(crate, crateMat);
 
 	auto ball = reg.create();
 	names.emplace(ball, "ball");
-	reg.emplace<TransformComponent>(ball, glm::mat4{1.0});
+	reg.emplace<TransformComponent>(ball, glm::vec3{ -10.0, 0.0, 10.0 });
 	reg.emplace<MeshComponent>(ball, ballMesh);
 	reg.emplace<MaterialComponent>(ball, ballMat);
 
 	auto floor = reg.create();
 	names.emplace(floor, "floor");
-	reg.emplace<TransformComponent>(floor, glm::mat4{1.0});
+	reg.emplace<TransformComponent>(floor, glm::vec3{ 0.0, 0.0, 0.0 });
 	reg.emplace<MeshComponent>(floor, floorMesh);
 	reg.emplace<MaterialComponent>(floor, floorMat);
+
+	auto sunMesh = Mesh::Create("sunMesh", "temp/plane.obj");
+	auto sunTex = Texture2D::Create("sunTex", "gw.png");
+	auto sunMat = Material::Create("sunMat", diffuseShader, sunTex);
+
+	auto sun = reg.create();
+	names.emplace(sun, "sun");
+	reg.emplace<TransformComponent>(sun, glm::vec3{ 0.0, 20.0, 0.0 });
+	reg.emplace<MeshComponent>(sun, sunMesh);
+	reg.emplace<MaterialComponent>(sun, sunMat);
 }
+
+template<typename... Components>
+void inspect_entity(entt::registry& registry, entt::entity entity) {
+	Logger::Writef(LogEntry::Level::Info, "{}", static_cast<uint32_t>(entity));
+
+	//expand pack to check which known components are attached
+	([&] {
+		if (registry.any_of<Components>(entity)) {
+			const auto& component = registry.get<Components>(entity);
+			std::cout << "  Component: " << typeid(Components).name() << '\n';
+			Logger::Writef(LogEntry::Level::Info, "{}", typeid(Components).name());
+
+			if constexpr (std::is_same_v<Components, TransformComponent>) {
+				Logger::Writef(LogEntry::Level::Info, "Position: ({}, {}, {})", component.localTransform[3][0], component.localTransform[3][1], component.localTransform[3][2]);
+			}
+			else if constexpr (std::is_same_v<Components, MeshComponent>) {
+				Logger::Writef(LogEntry::Level::Info, "Mesh: ()");
+			}
+			else if constexpr (std::is_same_v<Components, MaterialComponent>) {
+				Shader* tempShad = component.ptr->GetShader().get();
+				Texture* tempTex = component.ptr->GetTexture().get();
+				Logger::Writef(LogEntry::Level::Info, "Material: (Shader: {}, Texture: {})", fmt::ptr(tempShad), fmt::ptr(tempTex));
+			}
+		}
+	}(), ...); //expands over all component types
+}
+
 
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE, _In_ LPWSTR lpCmdLine, _In_ int nCmdShow) {
 
@@ -112,6 +149,10 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE, _In_ LPWSTR 
 
 	auto diffuseShader = Shader::Create("diffuse", "diffuse.vert.glsl", "diffuse.frag.glsl");
 	
+	for (auto entity : reg.storage<entt::entity>()) {
+		inspect_entity<TransformComponent, MeshComponent, MaterialComponent>(reg, entity);
+	}
+
 	ge.Run3D(reg);
 
 	return 0;
--- EngineCore/src/ecs/components/TransformComponent.h
+++ EngineCore/src/ecs/components/TransformComponent.h
@@ -7,7 +7,11 @@ namespace Cicada::ECS {
 	//TODO: consider combining hierarchical relationships with transform data, because you can't have one without the other
 	//what if theres an object in the hierarchy with no transform? completely screws up global transform calculation
 	struct TransformComponent {
-		glm::mat4 localTransform;
+		glm::mat4 localTransform = glm::mat4{1.0};
+
+		inline TransformComponent(const glm::vec3& position) {
+			localTransform[3] = glm::vec4(position, 1.0);			
+		}
 	};
 
 }
